eece0b180557c92b4f4e90f35cef1a5e1bd06b8b
AE <austence@sbcglobal.net>
2019-03-16 09:39:22 -0700

    updates main.js with working jquery script, updates .gitignore with correct lines, adds styles sheet link and  bootstrap script link to index.html, adds README.md.

---
+++ README.md
@@ -0,0 +1,17 @@
+# web102-san-diego-top-spots
+
+## Purpose of Program
+```
+Displays list of top activities in San Diego.
+```
+
+## Setup
+```
+$ npm install
+```
+
+## Run
+```
+$ static .
+```
+
--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
 // write your code here
+    $.getJSON("data.json", function(response) {
+        $.each(response, function(i, item) {
+        var $tr = $("<tr>").append(
+            $("<td>").text(item.name),
+            $("<td>").text(item.description),
+            $("<td>").append(
+            `<a href='https://www.google.com/maps?q=${item.location[0]},${item.location[1]}'>Directions</a>`
+            )
+        );
+        $tr.appendTo("#dataTable");
+        });
+    });
+    }); 
 
-});
+
+
+
+
